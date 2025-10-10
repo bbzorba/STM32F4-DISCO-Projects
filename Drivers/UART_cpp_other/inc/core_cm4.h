f3dcfd3b286c1e81c3f427879b080ecae6db7885
bbzorba <bbzorba@hotmail.com>
2025-10-10 11:46:01 +0200

    removed unused library

--- Drivers/UART_cpp_other/inc/core_cm4.h
+++
@@ -1,5 +0,0 @@
-#ifndef CORE_CM4_H
-#define CORE_CM4_H
-// Forward to the real CMSIS core header shipped with the repo
-#include "../../CMSIS/core_cm4.h"
-#endif // CORE_CM4_H
--- Drivers/UART_cpp_other/src/system_stm32f4xx.c
+++
@@ -1,8 +0,0 @@
-#include "stm32f4xx.h"
-
-uint32_t SystemCoreClock = 16000000UL; // Default HSI frequency
-
-void SystemInit(void) {
-    // Default: HSI 16MHz, no PLL
-    SystemCoreClock = 16000000UL;
-}
